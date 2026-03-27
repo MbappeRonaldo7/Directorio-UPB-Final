20d13227e57d59d646ebc1f2b04e69dc4b63af5d
MbappeRonaldo7 <tomasandresan7@gmail.com>
2026-03-27 01:29:43 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,31 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
 
+  const usuario = document.getElementById("usuario").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const mensaje = document.getElementById("mensaje");
+
+  mensaje.textContent = "";
+
+  // Simulación login
+  if (usuario === "admin" && password === "1234") {
+    mensaje.style.color = "green";
+    mensaje.textContent = "Ingreso correcto (Admin)";
+    
+    setTimeout(() => {
+      window.location.href = "admin.html";
+    }, 1000);
+
+  } else if (usuario === "usuario" && password === "1234") {
+    mensaje.style.color = "green";
+    mensaje.textContent = "Ingreso correcto (Usuario)";
+    
+    setTimeout(() => {
+      window.location.href = "privado.html";
+    }, 1000);
+
+  } else {
+    mensaje.style.color = "red";
+    mensaje.textContent = "Usuario o contraseña incorrectos";
+  }
+});
