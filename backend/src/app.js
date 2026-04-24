64f01c4dbdd7fbf3e2cb240a88bf6b54e3c79e83
MbappeRonaldo7 <tomasandresan7@gmail.com>
2026-04-24 04:36:18 -0500

    Fix cors definitivo

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,11 @@ const pool = require("./config/db");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST","PUT","DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(express.json());
 
 /* =====================================================
